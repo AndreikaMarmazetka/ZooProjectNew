e5d01589c1d78d8e211fcb021912da05594ef5eb
AndreikaMarmazetka <37284968+AndreikaMarmazetka@users.noreply.github.com>
2018-05-24 13:01:48 +0400

    New Version

---
+++ src/main/java/View/CreatePaddook.java
@@ -0,0 +1,48 @@
+package View;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class CreatePaddook extends JDialog {
+    private JPanel contentPane;
+    private JButton buttonOK;
+    private JButton buttonCancel;
+    private JCheckBox dangerousCheckBox;
+    private JTextField textEditType;
+    private JComboBox comboBoxFood;
+
+    public CreatePaddook() {
+        setContentPane(contentPane);
+        setModal(true);
+        getRootPane().setDefaultButton(buttonOK);
+
+        buttonOK.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onOK();
+            }
+        });
+
+        buttonCancel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        });
+
+
+    }
+
+    private void onOK() {
+        // add your code here
+        dispose();
+    }
+
+    private void onCancel() {
+        dispose();
+    }
+
+    public static void main(String[] args) {
+        CreatePaddook dialog = new CreatePaddook();
+        dialog.pack();
+        dialog.setVisible(true);
+    }
+}
